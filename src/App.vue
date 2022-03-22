b820275e0770a3eb7e22a23bc6bc0fed9df90079
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-03-22 18:13:54 +0100

    main components created

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
 <template>
   <div id="app">
-    <main-header />
+    <mainHeader />
+    <mainContent />
   </div>
 </template>
 
 <script>
 import mainHeader from './components/mainHeader.vue'
-// import mainContent from './components/main-content.vue'
+import mainContent from './components/mainContent.vue'
 // import mainFooter from './components/main-footer.vue'
 
 export default {
   name: 'App',
   components: {
     mainHeader,
-    // mainContent,
+    mainContent
     // mainFooter
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/styles/main";
+
 </style>
---
+++ src/components/mainContent.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+      <div class="container">
+
+          <h1>Content Goes Here!</h1>
+      </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'mainContent'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/main"; 
+main {
+    background-color: $mainBgContent;
+    color: white;
+    .container {
+        padding: 3rem 0;
+    }
+}
+</style>
